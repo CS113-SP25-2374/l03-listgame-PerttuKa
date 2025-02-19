28391e1c849053e5d25504f861b97b7da4aed987
MCCLab4804WD22 <pkaislasaari@gmail.com>
2025-02-18 20:45:48 -0800

    WIP

--- src/main/java/cs113/listGame/resources/ResourceArrayList.java
+++ src/main/java/cs113/listGame/resources/ResourceArrayList.java
@@ -2,15 +2,18 @@ package cs113.listGame.resources;
 
 import cs113.listGame.gamecore.GameObject;
 
+import java.util.ArrayList;
+
 public class ResourceArrayList implements ResourceList {
+    ArrayList<ResourceObject> resourceArray =  new ArrayList<>();
     @Override
     public void add(ResourceObject resource) {
-
+        resourceArray.add(resource);
     }
 
     @Override
     public void remove(ResourceObject resource) {
-
+        resourceArray.remove(resource);
     }
 
     @Override
